241923b5864ebacab2c45b12636dc439a4335d3b
Marco Domingos <66222324+Marco4763@users.noreply.github.com>
2023-06-05 20:38:15 +0100

    added hive implementation on project (#1)

--- lib/app/data/repository/users_repository_impl.dart
+++ lib/app/data/repository/users_repository_impl.dart
@@ -1,21 +1,30 @@
 import 'package:jsonplaceholder/app/data/datasource/users_data_source.dart';
 import 'package:jsonplaceholder/app/data/mappers/users_mappers.dart';
+import 'package:jsonplaceholder/app/data/service/hive/hive_impl.dart';
 import 'package:jsonplaceholder/app/domain/repository/users_repository.dart';
 import 'package:jsonplaceholder/app/domain/entities/users_entities.dart';
 
 class UsersRepositoryImpl implements UsersRepository {
+  final HiveServiceImpl<List<UsersEntities>> hiveServiceImpl;
   final UsersDataSource dataSource;
   final UserMapper mapper;
 
   UsersRepositoryImpl({
+    required this.hiveServiceImpl,
     required this.dataSource,
     required this.mapper,
   });
 
   @override
   Future<List<UsersEntities>> getUsers() {
-    return dataSource.getUsers().then((entity) {
-      return mapper.map(entity);
+    return dataSource.getUsers().then((entity) async {
+      final list = mapper.map(entity);
+      await hiveServiceImpl.initBox("userEntity");
+      await hiveServiceImpl.create("list", list);
+      return list;
+    }).catchError((e) async{
+      final list = await hiveServiceImpl.read("list") ?? [];
+      return list;
     });
   }
 }
--- lib/app/domain/entities/users_entities.dart
+++ lib/app/domain/entities/users_entities.dart
@@ -1,11 +1,24 @@
-class UsersEntities {
+import 'package:hive/hive.dart';
+
+part 'users_entities.g.dart';
+
+@HiveType(typeId: 0)
+class UsersEntities extends HiveObject {
+  @HiveField(0)
   int? id;
+  @HiveField(1)
   String? name;
+  @HiveField(2)
   String? username;
+  @HiveField(3)
   String? email;
+  @HiveField(4)
   AddressEntities? address;
+  @HiveField(5)
   String? phone;
+  @HiveField(6)
   String? website;
+  @HiveField(7)
   CompanyEntities? company;
 
   UsersEntities({
@@ -20,26 +33,39 @@ class UsersEntities {
   });
 }
 
-class AddressEntities {
+@HiveType(typeId: 1)
+class AddressEntities extends HiveObject{
+  @HiveField(0)
   String? street;
+  @HiveField(1)
   String? suite;
+  @HiveField(2)
   String? city;
+  @HiveField(3)
   String? zipcode;
+  @HiveField(4)
   GeoEntities? geo;
 
   AddressEntities({this.street, this.suite, this.city, this.zipcode, this.geo});
 }
 
-class GeoEntities {
+@HiveType(typeId: 2)
+class GeoEntities extends HiveObject{
+  @HiveField(0)
   String? lat;
+  @HiveField(1)
   String? lng;
 
   GeoEntities({this.lat, this.lng});
 }
 
-class CompanyEntities {
+@HiveType(typeId: 3)
+class CompanyEntities extends HiveObject{
+  @HiveField(0)
   String? name;
+  @HiveField(1)
   String? catchPhrase;
+  @HiveField(2)
   String? bs;
 
   CompanyEntities({this.name, this.catchPhrase, this.bs});
